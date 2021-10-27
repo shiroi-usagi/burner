7eb2cceb655abd4273122272d71295891ccda3d7
László Görög <contact@nergal.xyz>
2021-10-27 16:45:44 +0200

    ziputil: unzip files inmemory

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/shiroi-usagi/pkg v0.1.0
 	golang.org/x/mod v0.4.0
 )
+
+require github.com/jeffallen/seekinghttp v0.0.0-20171214161738-f41d11cb25b7 // indirect
--- ziputil/unzip.go
+++ ziputil/unzip.go
@@ -3,25 +3,48 @@ package ziputil
 import (
 	"archive/zip"
 	"fmt"
+	"github.com/jeffallen/seekinghttp"
 	"io"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
-// Unzip will decompress a zip archive, moving all files and folders
+// UnzipAll will decompress a zip archive, moving all files and folders
 // within the zip file (parameter 1) to an output directory (parameter 2).
 func UnzipAll(src string, dest string) ([]string, error) {
 	var filenames []string
 
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return filenames, err
+	var zr *zip.Reader
+	if strings.HasPrefix(src, "http://") || strings.HasPrefix(src, "https://") {
+		body := seekinghttp.New(src)
+		size, err := body.Size()
+		if err != nil {
+			return nil, err
+		}
+		r, err := zip.NewReader(body, size)
+		if err != nil {
+			return nil, err
+		}
+		zr = r
+	} else {
+		f, err := os.Open(src)
+		if err != nil {
+			return nil, err
+		}
+		defer f.Close()
+		fi, err := f.Stat()
+		if err != nil {
+			return nil, err
+		}
+		r, err := zip.NewReader(f, fi.Size())
+		if err != nil {
+			return nil, err
+		}
+		zr = r
 	}
-	defer r.Close()
 
-	for _, f := range r.File {
-		// Store filename/path for returning and using later on
+	for _, f := range zr.File {
 		fpath := filepath.Join(dest, f.Name)
 
 		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
@@ -32,32 +55,25 @@ func UnzipAll(src string, dest string) ([]string, error) {
 		filenames = append(filenames, fpath)
 
 		if f.FileInfo().IsDir() {
-			// Make Folder
 			os.MkdirAll(fpath, os.ModePerm)
 			continue
 		}
 
-		// Make File
-		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+		if err := os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
 			return filenames, err
 		}
-
 		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
 		if err != nil {
 			return filenames, err
 		}
-
 		rc, err := f.Open()
 		if err != nil {
+			outFile.Close()
 			return filenames, err
 		}
-
 		_, err = io.Copy(outFile, rc)
-
-		// Close the file without defer to close before next iteration of loop
 		outFile.Close()
 		rc.Close()
-
 		if err != nil {
 			return filenames, err
 		}
